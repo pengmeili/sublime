c5ad6a81117f003083eb0b1b10a00d3a30ba833c
pengml1 <pengml1@lenovo.com>
2020-09-18 18:12:36 +0800

    update

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Switch, Route, HashRouter} from 'react-router-dom'
+import Home from "./views/Home";
+import About from "./views/About";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <HashRouter>
+        <Switch>
+          <Route component={Home} exact path="/"></Route>
+          <Route component={About} path="/about"></Route>
+        </Switch>
+      </HashRouter>
+    );
+  }
 }
-
 export default App;
